09a9d85d2b78a18a95fa9d74f4c7f00ceb331981
pedroarneiroz <pedroarneiroz@gmail.com>
2023-04-12 23:39:04 -0300

    implements service httpClient and interpolations

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { PokeApiService } from './../../service/poke-api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+
+  public getAllPokemons: any;
+
+  constructor(
+    private pokeApiService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results;
+        console.log(this.getAllPokemons)
+
+      }
+    );
+  }
 
 }
